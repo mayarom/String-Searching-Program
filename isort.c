c3865eab5e5d394743ad4dd049b36de993feb192
maya <maya5660@gmail.com>
2023-01-07 23:37:40 +0200

    isortfix

--- isort.c
+++ isort.c
@@ -27,13 +27,13 @@ int main()
 
 void shift_element(int *arr, int i)
 { // shifts the elements of the array to the right
-    for (int item = i; item > 0; --item)
+    for (int item = i; item > 0; item--)
         *(arr + item) = *(arr + item - 1);
 }
 
 void insertion_sort(int *arr, int len)
 {
-    for (int i = 1; i < len; ++i)
+    for (int i = 1; i < len; i++)
     {
         int key = *(arr + i), j = (i - 1);
 
